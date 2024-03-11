7502460266400228c175a3cec0ef49785fbbcd37
Al Stone <astone@heckgrammar.co.uk>
2024-03-11 10:47:28 +0000

    Add project files.

---
+++ BinaryToolVBTask/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+
+    End Sub
+    Function binaryToDenary(binaryNum As String) As Integer
+
+    End Function
+    Function denaryToBinary(denaryNum As Integer) As String
+
+    End Function
+    Function binaryShift(binaryNum As String, shift As Integer)
+
+    End Function
+End Module
